299c13e8f9edceb1026182455435888041e319dd
Digital Villa <villanueva1077@gmail.com>
2018-01-24 13:14:49 -0700

    version 1.2 Autoprefixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-console.log ("conected");
+console.log("conected");
 
-var btn = Document.
+var menu = document.querySelector("#nav-toggle");
+
+document.addEventListener('click', function (e) {
+    if (e.target.id == 2 || e.target.id == 3 || e.target.id == 4 || e.target.id == 5) {
+        menu.checked = false;
+    }
+});
