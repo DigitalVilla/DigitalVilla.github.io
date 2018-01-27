72ba6f0176c3ddf5d207998e4110378ab2684a0a
Digital Villa <villanueva1077@gmail.com>
2018-01-27 12:19:23 -0700

    JS added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ document.addEventListener('click', function (e) {
         menu.checked = false;
     }
 });
+
+
+// var p = document.getElementsByTagName('p')[0];
+// p.onclick = function() {
+//  // Trigger the `hover` event on the paragraph
+//  p.onhover.call(p);
+// };
+
+
+
+//mobile function
+
+// $(document).ready(function() {
+//     $('.hover').bind('touchstart touchend', function(e) {
+//         e.preventDefault();
+//         $(this).toggleClass('hover_effect');
+//     });
+// });
+var lightbox = document.getElementsByClassName("lightbox_photo");
+lightbox.onclick = function () {
+    lightbox.onhover.call(lightbox);
+};
