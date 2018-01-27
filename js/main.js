c8f0da952969e483a5a7accb8b1a337238ed911d
Digital Villa <villanueva1077@gmail.com>
2018-01-27 00:15:56 -0700

    fixing respomnive design isues onfd old phones

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log("conected");
 var menu = document.querySelector("#nav-toggle");
 
 document.addEventListener('click', function (e) {
-    if (e.target.id == 2 || e.target.id == 3 || e.target.id == 4 || e.target.id == 5) {
+    if (e.target.id >= 1 && e.target.id <= 6) {
         menu.checked = false;
     }
 });
