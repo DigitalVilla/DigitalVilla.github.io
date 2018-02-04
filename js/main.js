c098c49ac86e7039ec57198d822f349e1a4e7c02
Digital Villa <villanueva1077@gmail.com>
2018-02-04 02:32:48 -0700

    Slide show added

--- js/main.js
+++ js/main.js
@@ -20,3 +20,35 @@ var lightbox = document.getElementsByClassName("lightbox_photo");
 lightbox.onclick = function () {
     lightbox.onhover.call(lightbox);
 };
+
+//Slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusDivs(indx) {
+    showSlides(slideIndex += indx);
+}
+
+function currentDiv(indx) {
+    showSlides(slideIndex = indx);
+}
+
+function showSlides(indx) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("slider-nav-pager-divs");
+    if (indx > slides.length) {
+        slideIndex = 1
+    }
+    if (indx < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("current", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " current";
+}
